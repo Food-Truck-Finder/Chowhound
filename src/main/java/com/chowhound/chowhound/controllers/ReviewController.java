b4f2a7b52cdcef5a26e32e121e3af8d6afff3895
Jeff Walton <jeff.walton205@gmail.com>
2020-06-01 11:44:41 -0500

    Completed get mapping for review

--- src/main/java/com/chowhound/chowhound/controllers/ReviewController.java
+++ src/main/java/com/chowhound/chowhound/controllers/ReviewController.java
@@ -1,10 +1,14 @@
 package com.chowhound.chowhound.controllers;
 
+import com.chowhound.chowhound.models.Truck;
 import com.chowhound.chowhound.repos.ReviewRepo;
 import com.chowhound.chowhound.repos.TruckRepo;
 import com.chowhound.chowhound.repos.UserRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ReviewController {
@@ -23,7 +27,13 @@ public class ReviewController {
     }
 
     //Mapping to get review page
-    @GetMapping ("/trucks/{id}/review")
+    @GetMapping ("/reviews/{id}/addReview")
+    public String GetReviewForm (
+            @ModelAttribute Truck truck, @PathVariable ("id") Long truckId, Model model
+    ) {
+        truck = truckRepo.getOne(truckId);
+        return "trucks/review";
+    }
 
 
     //Mapping to post review page
