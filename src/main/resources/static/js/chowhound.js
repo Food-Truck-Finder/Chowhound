f1296ae948a4cd6a7889d1122decf7d47177bde2
Austin Gray <Austin.m.gray210@gmail.com>
2020-06-01 19:09:26 -0500

    changed layout of /create page

--- src/main/resources/static/js/chowhound.js
+++ src/main/resources/static/js/chowhound.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const client = filestack.init(/*ADD KEY HERE*/);
+const client = filestack.init("AiFOiEr0bQsu4CEAfBngWz");
 const options = {
     onUploadDone: updateForm,
     maxSize: 10 * 1920 * 1080,
