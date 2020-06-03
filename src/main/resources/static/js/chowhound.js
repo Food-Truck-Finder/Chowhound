9a1959ef81bbc4968f46b53a223a433b0cbe1e67
Austin Gray <Austin.m.gray210@gmail.com>
2020-06-03 18:06:51 -0500

    placeholder image functionality finished

--- src/main/resources/static/js/chowhound.js
+++ src/main/resources/static/js/chowhound.js
@@ -1,53 +1,86 @@
 "use strict";
+$(document).ready(function () {
 
-const client = filestack.init(fileStackKey);
-const options = {
-    onUploadDone: updateForm,
-    maxSize: 10 * 1920 * 1080,
-    accept: 'image/*',
-    uploadInBackground: false,
-};
-const picker = client.picker(options);
+    /* Only runs filestack API logic if in a window that we have the upload button in */
+    /* Doing this to avoid console errors and so it doesn't run when not needed */
+    if (window.location.href.indexOf("trucks") > -1) {
 
-// Get references to the DOM elements
+        const client = filestack.init(fileStackKey);
+        const options = {
+            onUploadDone: updateForm,
+            maxSize: 10 * 1920 * 1080,
+            accept: 'image/*',
+            uploadInBackground: false,
+        };
 
-const form = document.getElementById('pick-form');
-const fileInput = document.getElementById('fileupload');
-const btn = document.getElementById('picker');
-const nameBox = document.getElementById('nameBox');
-const urlBox = document.getElementById('urlBox');
+        /* Get references to the DOM elements */
+        const picker = client.picker(options);
+        // const form = document.getElementById('pick-form');
+        // const fileInput = document.getElementById('fileupload');
+        const btn = document.getElementById('picker');
+        // const nameBox = document.getElementById('nameBox');
+        // const urlBox = document.getElementById('urlBox');
 
-// Add our event listeners
+        /* Event Listener */
+        btn.addEventListener('click', function (e) {
+            e.preventDefault();
+            picker.open();
+        });
 
-btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    picker.open();
-});
+        /* Helper to overwrite the field input value */
+        function updateForm(result) {
+            const fileData = result.filesUploaded[0];
+            console.log(fileData.url);
+            $("#imageURL").val(fileData.url);
+            showSaveButtonAndHideUpload();
+        }
+    }
 
-// Helper to overwrite the field input value
-
-function updateForm(result) {
-    const fileData = result.filesUploaded[0];
-    console.log(fileData.url);
-    $("#imageURL").val(fileData.url);
-    alert("Image successfully uploaded.");
-}
-
-function showOwnerRegisterInfo() {
+    /* NEW TRUCK EVENT TO SHOW AND HIDE ADDITIONAL INFO */
+    function showOwnerRegisterInfo() {
         $("#ownerEmail").removeClass("hidden");
         $("#ownerPhoneNumber").removeClass("hidden");
         $("#ownerTwitterUrl").removeClass("hidden");
         $("#ownerFacebookUrl").removeClass("hidden");
         $("#ownerInstagramUrl").removeClass("hidden");
-}
+    }
 
-function hideOwnerRegistrationInfo() {
+    function hideOwnerRegistrationInfo() {
         $("#ownerEmail").addClass("hidden");
         $("#ownerPhoneNumber").addClass("hidden");
         $("#ownerTwitterUrl").addClass("hidden");
         $("#ownerFacebookUrl").addClass("hidden");
         $("#ownerInstagramUrl").addClass("hidden");
-}
+    }
+
+    /* TRUCK VIEW EVENT TO HIDE UPLOAD PIC & SHOW SAVE BUTTON */
+    function showSaveButtonAndHideUpload() {
+        $("#truckImageAddPictureButton").addClass("hidden");
+        $("#saveImg").removeClass("hidden");
+    }
+
+    /* Placeholder image logic */
+    /* image will no longer show up if an image uploaded .isPrimary() */
+    let numberOfTrucks = $("#mainTruckContainer").children().length;
+    for (let i = 1; i <= numberOfTrucks; i++) {
+        if (document.getElementById("truck_" + i)) {
+            if (document.getElementById("truck_" + i).childNodes.item(1).childNodes.item(1).childNodes.item(1).childNodes.item(1).children.item(0) === null) {
+                $("#truck" + i + "carousel").append(
+                    '<div id="' + 'truck_' + i + '_primaryImg" ' +
+                    'class="' + 'carousel-item active mainCarouselImage">' +
+                    '<img class="' + 'd-block w-100 cardImageFixed" ' +
+                    'src="' + '../images/placeholderImg.png" ' +
+                    'alt="' + 'placeholderImg">' +
+                    '</div>'
+                );
+            }
+        }
+    }
+
+});
+
+
+
 
 
 
