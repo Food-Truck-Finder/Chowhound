e72d7705652d8866cc3b174eb63aa3e6a1e54004
Austin Gray <Austin.m.gray210@gmail.com>
2020-06-04 11:23:53 -0500

    removed document.ready out of JS, added logic to file picker make sure user is logged in to get rid of console errors, added ID to partials logout button to be targetable for file picker logic

--- src/main/resources/static/js/chowhound.js
+++ src/main/resources/static/js/chowhound.js
@@ -1,77 +1,74 @@
 "use strict";
 
-$(document).ready(function () {
+/* Only runs filestack API logic if in a window that we have the upload button in */
+/* Doing this to avoid console errors and so it doesn't run when not needed */
+if ((window.location.href.indexOf("trucks") > -1) && (!($("#logoutButton")))) {
 
-    /* Only runs filestack API logic if in a window that we have the upload button in */
-    /* Doing this to avoid console errors and so it doesn't run when not needed */
-    if (window.location.href.indexOf("trucks") > -1) {
+    const client = filestack.init(fileStackKey);
+    const options = {
+        onUploadDone: updateForm,
+        maxSize: 10 * 1920 * 1080,
+        accept: 'image/*',
+        uploadInBackground: false,
+    };
 
-        const client = filestack.init(fileStackKey);
-        const options = {
-            onUploadDone: updateForm,
-            maxSize: 10 * 1920 * 1080,
-            accept: 'image/*',
-            uploadInBackground: false,
-        };
+    /* Get references to the DOM elements */
+    const picker = client.picker(options);
+    // const form = document.getElementById('pick-form');
+    // const fileInput = document.getElementById('fileupload');
+    const btn = document.getElementById('picker');
+    // const nameBox = document.getElementById('nameBox');
+    // const urlBox = document.getElementById('urlBox');
 
-        /* Get references to the DOM elements */
-        const picker = client.picker(options);
-        // const form = document.getElementById('pick-form');
-        // const fileInput = document.getElementById('fileupload');
-        const btn = document.getElementById('picker');
-        // const nameBox = document.getElementById('nameBox');
-        // const urlBox = document.getElementById('urlBox');
+    /* Event Listener */
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        picker.open();
+    });
 
-        /* Event Listener */
-        btn.addEventListener('click', function (e) {
-            e.preventDefault();
-            picker.open();
-        });
+    /* Helper to overwrite the field input value */
+    function updateForm(result) {
+        const fileData = result.filesUploaded[0];
+        console.log(fileData.url);
+        $("#imageURL").val(fileData.url);
+        showSaveButtonAndHideUpload();
+    }
+}
 
-        /* Helper to overwrite the field input value */
-        function updateForm(result) {
-            const fileData = result.filesUploaded[0];
-            console.log(fileData.url);
-            $("#imageURL").val(fileData.url);
-            showSaveButtonAndHideUpload();
+/* Placeholder image logic */
+/* image will no longer show up if an image uploaded .isPrimary() */
+let numberOfTrucks = $("#mainTruckContainer").children().length;
+for (let i = 1; i <= numberOfTrucks; i++) {
+    if ($("#truck_" + i)) {
+        if (($("#truck_" + i).children().children().children().children().children().hasClass("active") === false)
+            && ($("#truck_" + i).children().children().children().children().children().hasClass("secondaryCarouselImage") === true)) {
+            let secondaryToPrimaryImage = $("#truck_" + i).children().children().children().children().children().first()[0].lastElementChild.currentSrc;
+            $("#truck" + i + "carousel").append(
+                "<div id=\"truck_" + i + "_primaryImg\" " +
+                "class=\"carousel-item active mainCarouselImage\">" +
+                "<img class=\"d-block w-100 cardImageFixed\" " +
+                "src=\"" + secondaryToPrimaryImage + "\"" +
+                "alt=\"temporaryImage\">" +
+                "</div>"
+            );
+            document.querySelector("#truck" + i + "carousel > div.carousel-item.secondaryCarouselImage").remove();
+        } else if (($("#truck_" + i).children().children().children().children().children().hasClass("active") === false)
+            && ($("#truck_" + i).children().children().children().children().children().hasClass("secondaryCarouselImage") === false)) {
+            $("#truck" + i + "carousel").append(
+                "<div id=\"truck_" + i + "_primaryImg\" " +
+                "class=\"carousel-item active mainCarouselImage\">" +
+                "<img class=\"d-block w-100 cardImageFixed\" " +
+                "src=\"../images/placeholderImg.png\" " +
+                "alt=\"placeholderImg\">" +
+                "</div>"
+            );
         }
     }
-
-    /* Placeholder image logic */
-    /* image will no longer show up if an image uploaded .isPrimary() */
-    let numberOfTrucks = $("#mainTruckContainer").children().length;
-    for (let i = 1; i <= numberOfTrucks; i++) {
-        if ($("#truck_" + i)) {
-            if (($("#truck_" + i).children().children().children().children().children().hasClass("active") === false)
-                && ($("#truck_" + i).children().children().children().children().children().hasClass("secondaryCarouselImage") === true)) {
-                let secondaryToPrimaryImage = $("#truck_" + i).children().children().children().children().children().first()[0].lastElementChild.currentSrc;
-                $("#truck" + i + "carousel").append(
-                    "<div id=\"truck_" + i + "_primaryImg\" " +
-                    "class=\"carousel-item active mainCarouselImage\">" +
-                    "<img class=\"d-block w-100 cardImageFixed\" " +
-                    "src=\"" + secondaryToPrimaryImage + "\"" +
-                    "alt=\"temporaryImage\">" +
-                    "</div>"
-                );
-                document.querySelector("#truck" + i + "carousel > div.carousel-item.secondaryCarouselImage").remove();
-            } else if (($("#truck_" + i).children().children().children().children().children().hasClass("active") === false)
-                && ($("#truck_" + i).children().children().children().children().children().hasClass("secondaryCarouselImage") === false)) {
-                $("#truck" + i + "carousel").append(
-                    "<div id=\"truck_" + i + "_primaryImg\" " +
-                    "class=\"carousel-item active mainCarouselImage\">" +
-                    "<img class=\"d-block w-100 cardImageFixed\" " +
-                    "src=\"../images/placeholderImg.png\" " +
-                    "alt=\"placeholderImg\">" +
-                    "</div>"
-                );
-            }
-        }
-    }
-});
+}
 
 /* NEW TRUCK EVENT TO SHOW AND HIDE ADDITIONAL INFO */
 function showOwnerRegisterInfo() {
-    $("#primaryImg").removeClass("hidden");
+    $("#primaryImgUploadButton").removeClass("hidden");
     $("#ownerPhoneNumber").removeClass("hidden");
     $("#ownerTwitterUrl").removeClass("hidden");
     $("#ownerFacebookUrl").removeClass("hidden");
@@ -79,7 +76,7 @@ function showOwnerRegisterInfo() {
 }
 
 function hideOwnerRegistrationInfo() {
-    $("#primaryImg").addClass("hidden");
+    $("#primaryImgUploadButton").addClass("hidden");
     $("#ownerPhoneNumber").addClass("hidden");
     $("#ownerTwitterUrl").addClass("hidden");
     $("#ownerFacebookUrl").addClass("hidden");
